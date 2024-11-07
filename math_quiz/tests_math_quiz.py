f95dc5cb6ee5dd21907d0f3f4203d852cf444196
Gojo George <jojogeo@gmail.com>
2024-11-07 11:41:48 +0100

    commit after modified test_math_quiz.py with various test cases added

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,34 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import Rand_Int, Math_Operator, Math_Operation
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_Rand_Int(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = Rand_Int(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
+    def test_Math_Operator(self):
         # TODO
         pass
 
-    def test_function_C(self):
+    def test_Math_Operation(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (12, 4, '*','12 * 4', 48),
+                (50, 10, '-', '50 - 10', 40)
+                
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                p, a = Math_Operation(num1, num2, operator)
+                self.assertEqual(p, expected_problem)
+                self.assertEqual(a, expected_answer)
+                
 
 if __name__ == "__main__":
     unittest.main()

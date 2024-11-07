c2b77303400652026c3999a8666a9d423da34765
Gojo George <jojogeo@gmail.com>
2024-11-07 13:37:58 +0100

    commit after adding files for making pip installable package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='math_quiz',
+    version='1.0.0',
+    description='DSSS homework 2',
+    url='https://github.com/gojogeo/dsss_homework_2.git',
+    author='Gojo George'
+    
+)

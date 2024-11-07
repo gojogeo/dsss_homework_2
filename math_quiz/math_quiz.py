6c7cba60cff2b70bc63c40ddb014f34142ce4e95
Gojo George <jojogeo@gmail.com>
2024-11-07 11:12:56 +0100

    commit made after cleaning up the code math_quiz.py

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,42 +1,80 @@
 import random
 
 
-def function_A(min, max):
+def Rand_Int(min, max):
     """
-    Random integer.
+    Returns a random number between min and max.
+
+    :param min: The minimum value to return
+    :type min: int
+    :param max: The maximum value to return
+    :type max: int
+    :return: A random integer between min and max
+    :rtype: int
+    
     """
     return random.randint(min, max)
 
 
-def function_B():
+def Math_Operator():
+    """
+    Returns a random mathematical opertation 
+    :return: A random mathematical opertation between '+','-', and '*'
+    :rtype: string
+
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
+def Math_Operation(n1, n2, o):
+    """
+    Returns a mathematical expression and its result
+
+    :param n1: The first number
+    :type n1: int
+    :param n2: The second number
+    :type n2: int
+    :param o: The operator
+    :type o: string
+    :return p: string of mathematical expression
+    :type p: string
+    :return a: result of the evaluated mathematical expression
+    :type a: int
+
+    """
     p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
+    if o == '+': a = n1 + n2
+    elif o == '-': a = n1 - n2
     else: a = n1 * n2
     return p, a
 
 def math_quiz():
+    """
+    Math quiz, where user is presented with a mathematical expression t_q times. User is prompted to 
+    input the result. The result is compared with the correct answer and points are added to the score.
+    In the end of the quiz user is presented with the final score
+    """
     s = 0
-    t_q = 3.14159265359
+    t_q = 4
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        n1 = Rand_Int(1, 10); n2 = Rand_Int(1, 20); o = Math_Operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = Math_Operation(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        try:
+            useranswer = input("Your answer: ")
+            useranswer = int(useranswer)
+        except:
+            print ("Invalid Entry. Enter an integer number")
+            pass
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            s += 1
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
